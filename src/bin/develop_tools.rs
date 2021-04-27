71f9e8185ca254d91f722eabfdffb3bb9a654496
simonjiao <simon_jiaoh@hotmail.com>
2021-04-27 15:29:57 +0800

    logging done

--- src/bin/develop_tools.rs
+++ src/bin/develop_tools.rs
@@ -1,7 +1,35 @@
-use learning_rust::execute_query;
+fn main() {
+    //use env_logger::{Builder, Target};
+    //use learning_rust::execute_query;
+    //use learning_rust::execute_query_err;
+    //env_logger::init();
+    //Builder::new().target(Target::Stdout).init();
 
-fn main(){
-    env_logger::init();
+    //log::error!("This error has been sent to stdout");
 
-    execute_query("DROP TABLE students");
+    //execute_query("DROP TABLE students");
+
+    //if let Err(e) = execute_query_err("Drop") {
+    //    log::error!("Failed to execute query: {}", e);
+    //}
+
+    //use learning_rust::ConsoleLogger;
+    //use log::LevelFilter;
+
+    //static CONSOLE_LOGGER:ConsoleLogger = ConsoleLogger;
+
+    //if let Err(e) = log::set_logger(&CONSOLE_LOGGER) {
+    //    println!("Failed to set logger {}", e)
+    //}
+    //log::set_max_level(LevelFilter::Info);
+
+    //log::debug!("debug");
+    //log::info!("info");
+    //log::warn!("warn");
+    //log::error!("error");
+
+    use learning_rust::log_to_syslog;
+    if let Err(e) = log_to_syslog() {
+        println!("Error: {}", e);
+    }
 }
--- src/develop_tools.rs
+++ src/develop_tools.rs
@@ -1,3 +1,39 @@
+use log::Level;
+
 pub fn execute_query(query: &str) {
     log::debug!("Executing query: {}", query);
+}
+
+pub fn execute_query_err(_query: &str) -> Result<(), &'static str> {
+    Err("I'm afraid I can't do that")
+}
+
+use log::{Metadata, Record};
+pub struct ConsoleLogger;
+
+impl log::Log for ConsoleLogger {
+    fn enabled(&self, metadata:&Metadata) -> bool {
+        metadata.level() <= Level::Info
+    }
+
+    fn log(&self, record:&Record) {
+        if self.enabled(record.metadata()) {
+            println!("Rust says: {}: {:?}", record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+use syslog::{Facility, Error};
+
+pub fn log_to_syslog() -> Result<(), Error>{
+    syslog::init(Facility::LOG_USER,
+    log::LevelFilter::Debug,
+    Some("My app name"))?;
+
+    log::debug!("this is a debug {}", "message");
+    log::error!("this is an error");
+
+    Ok(())
 }
