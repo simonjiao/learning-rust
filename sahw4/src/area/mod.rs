ced282a5e09db3f0d1657548037169b6dab93e26
simonjiao <simon@findora.org>
2022-04-13 20:23:30 +0800

    refactor area

--- sahw4/src/area/mod.rs
+++ sahw4/src/area/mod.rs
@@ -1,12 +1,7 @@
-use std::f64::consts::PI;
-
-pub trait Shape {
-    fn area(&self) -> f64;
-
-    fn valid(&self) -> bool {
-        true
-    }
-}
+pub use circle::Circle;
+pub use rectangle::Square;
+pub use shape::Shape;
+pub use triangle::Triangle;
 
 pub fn area<T>(shape: &T) -> f64
 where
@@ -15,105 +10,139 @@ where
     shape.area()
 }
 
-#[derive(Debug)]
-pub struct Circle(f64);
-#[derive(Debug)]
-pub struct Triangle(f64, f64, f64);
-#[derive(Debug)]
-pub struct Square(f64);
+mod shape {
+    pub trait Shape {
+        fn area(&self) -> f64;
 
-impl Circle {
-    pub fn new(radius: f64) -> Self {
-        Self(radius)
+        fn valid(&self) -> bool {
+            true
+        }
     }
 }
 
-impl Shape for Circle {
-    fn area(&self) -> f64 {
-        PI * self.0 * self.0
+mod circle {
+    use super::shape::*;
+    use std::f64::consts::PI;
+
+    #[derive(Debug)]
+    pub struct Circle(f64);
+
+    impl Circle {
+        pub fn new(radius: f64) -> Self {
+            Self(radius)
+        }
     }
 
-    fn valid(&self) -> bool {
-        self.0 > 0f64
-    }
-}
+    impl Shape for Circle {
+        fn area(&self) -> f64 {
+            PI * self.0 * self.0
+        }
 
-impl Triangle {
-    pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
-        Self(e0, e1, e2)
-    }
-}
-
-impl Shape for Triangle {
-    fn area(&self) -> f64 {
-        let hp = (self.0 + self.1 + self.2) / 2f64;
-
-        (hp * (hp - self.0) * (hp - self.1) * (hp - self.2)).sqrt()
+        fn valid(&self) -> bool {
+            self.0 > 0f64
+        }
     }
 
-    fn valid(&self) -> bool {
-        let v0 = self.0 + self.1 > self.2;
-        let v1 = self.0 + self.2 > self.1;
-        let v2 = self.1 + self.2 > self.0;
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn circle_case() {
+            let c = Circle::new(1f64);
+            assert!(c.valid());
+            assert_eq!(c.area(), PI * 1f64 * 1f64);
 
-        let v3 = self.0 > 0f64 && self.1 > 0f64 && self.2 > 0f64;
-
-        v0 && v1 && v2 && v3
-    }
-}
-
-impl Square {
-    pub fn new(e: f64) -> Self {
-        Self(e)
+            assert!(!Circle::new(0f64).valid());
+        }
     }
 }
 
-impl Shape for Square {
-    fn area(&self) -> f64 {
-        self.0 * self.0
+mod triangle {
+    use super::shape::*;
+
+    #[derive(Debug)]
+    pub struct Triangle(f64, f64, f64);
+
+    impl Triangle {
+        pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
+            Self(e0, e1, e2)
+        }
     }
 
-    fn valid(&self) -> bool {
-        self.0 > 0f64
+    impl Shape for Triangle {
+        fn area(&self) -> f64 {
+            let hp = (self.0 + self.1 + self.2) / 2f64;
+
+            (hp * (hp - self.0) * (hp - self.1) * (hp - self.2)).sqrt()
+        }
+
+        fn valid(&self) -> bool {
+            let v0 = self.0 + self.1 > self.2;
+            let v1 = self.0 + self.2 > self.1;
+            let v2 = self.1 + self.2 > self.0;
+
+            let v3 = self.0 > 0f64 && self.1 > 0f64 && self.2 > 0f64;
+
+            v0 && v1 && v2 && v3
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn triangle_case() {
+            assert!(!Triangle::new(0f64, 0f64, 0f64).valid());
+            assert!(!Triangle::new(1f64, 2f64, 0f64).valid());
+
+            assert!(!Triangle::new(1f64, 2f64, 3f64).valid());
+            assert!(!Triangle::new(1f64, 3f64, 2f64).valid());
+            assert!(!Triangle::new(2f64, 3f64, 1f64).valid());
+            assert!(!Triangle::new(2f64, 3f64, 1f64).valid());
+            assert!(!Triangle::new(3f64, 2f64, 1f64).valid());
+            assert!(!Triangle::new(3f64, 1f64, 2f64).valid());
+
+            let x = Triangle::new(3f64, 4f64, 5f64);
+            assert!(x.valid());
+            assert_eq!(x.area(), 6f64);
+        }
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod rectangle {
+    use super::shape::*;
 
-    #[test]
-    fn circle_case() {
-        let c = Circle::new(1f64);
-        assert!(c.valid());
-        assert_eq!(c.area(), PI * 1f64 * 1f64);
+    #[derive(Debug)]
+    pub struct Square(f64);
 
-        assert!(!Circle::new(0f64).valid());
+    impl Square {
+        pub fn new(e: f64) -> Self {
+            Self(e)
+        }
     }
 
-    #[test]
-    fn triangle_case() {
-        assert!(!Triangle::new(0f64, 0f64, 0f64).valid());
-        assert!(!Triangle::new(1f64, 2f64, 0f64).valid());
+    impl Shape for Square {
+        fn area(&self) -> f64 {
+            self.0 * self.0
+        }
 
-        assert!(!Triangle::new(1f64, 2f64, 3f64).valid());
-        assert!(!Triangle::new(1f64, 3f64, 2f64).valid());
-        assert!(!Triangle::new(2f64, 3f64, 1f64).valid());
-        assert!(!Triangle::new(2f64, 3f64, 1f64).valid());
-        assert!(!Triangle::new(3f64, 2f64, 1f64).valid());
-        assert!(!Triangle::new(3f64, 1f64, 2f64).valid());
-
-        let x = Triangle::new(3f64, 4f64, 5f64);
-        assert!(x.valid());
-        assert_eq!(x.area(), 6f64);
+        fn valid(&self) -> bool {
+            self.0 > 0f64
+        }
     }
 
-    #[test]
-    fn square_case() {
-        assert!(!Square::new(0f64).valid());
+    #[cfg(test)]
+    mod tests {
+        use super::*;
 
-        let x = Square::new(10f64);
-        assert!(x.valid());
-        assert_eq!(x.area(), 100f64);
+        #[test]
+        fn square_case() {
+            assert!(!Square::new(0f64).valid());
+
+            let x = Square::new(10f64);
+            assert!(x.valid());
+            assert_eq!(x.area(), 100f64);
+        }
     }
 }
